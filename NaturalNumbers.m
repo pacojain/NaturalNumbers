8a6553d0ffe957de155ad1bc365c441d6c2ea984
Paco Jain <pacojain@gmail.com>
2014-12-22 17:46:50 -0600

    Set up as an eclipse project and added boilerplate NaturalNumbers.m file

---
+++ NaturalNumbers.m
@@ -0,0 +1,18 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench Nov Dec 22, 2014 
+   Author: Paco Jain (pacojain@gmail.com)
+*)
+
+BeginPackage["NaturalNumbers`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+
+End[]
+
+EndPackage[]
