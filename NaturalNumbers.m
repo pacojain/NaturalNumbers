9af58cffc4f8046735e6ac4c01df6a4a94b79420
Paco Jain <pacojain@gmail.com>
2014-12-23 00:16:49 -0600

    Crystallized separation between abstraction and implimentation. Introduced mechanism (not entirely successful) for switching between abstract and concrete representations.

--- NaturalNumbers.m
+++ NaturalNumbers.m
@@ -9,21 +9,40 @@ BeginPackage["NaturalNumbers`"]
 
 Zero::usage= "Zero represents the first natural number (zero) in the Peano axiomatic definition of the natural numbers"
 Successor::usage= "Successor[NaturalNumber[n]] represents of the successor of NaturalNumber[n] in the sense of the Peano axioms"
+Predecessor::usage= "Inverse relationship to successor (Predecessor[Zero] is Undefined)"
 Add::usage= "Represents two-argument addtion for conceptual natural numbers"
 NaturalNumber::usage= "NaturalNumber[n_Integer] is the representation of the natural number n in the conceptual reformulation"
+$NaturalNumbersUseRealization::usage= "If True, use the NaturalNumbers datatype as a concrete normal-form representation.  Otherwise, use abstract Successor[Successor[...Zero]] forms"
+
 
 Begin["`Private`"]
 (* Implementation of the package *)
 
-(* Representation-independent defintion of Addition of natural numbers *)
-Add[n_, Zero]:= n
-Add[n_, Successor[m_]]:= Successor[Add[n, m]]
+(* Basic definitions: Successor, Predecessor, and Zero *)
+Predecessor[Zero]:= Undefined
+Successor[Undefined]:= Undefined
+Predecessor[Undefined]:= Undefined
+Successor[Predecessor[n:Except[Zero]]]:= n
+Predecessor[Successor[n_]]:= n
 
-(* Realization of Addition through abstract "NaturalNumbers" datatype *)
-Zero:= NaturalNumber[0]
-Successor[NaturalNumber[n_Integer]]:= NaturalNumber[n+1]
-Add[NaturalNumber[n_Integer], NaturalNumber[m_Integer]]:= Successor[ Add[NaturalNumber[n], NaturalNumber[m-1]] ]
-Add[NaturalNumber[n_Integer], NaturalNumber[0]]:= NaturalNumber[n]
+NNpattern::usage= "A pattern used to define arithmetic in this package which changes based on value of $NaturalNumbersUseRealization"
+If[$NaturalNumbersUseRealization, NNpattern=NaturalNumber[_Integer], NNpattern=_] 
+(* Representation-independent defintion of Addition of natural numbers *)
+Add[n: NNpattern, Zero]:= n
+Add[n: NNpattern, m: NNpattern]:= Successor[ Add[n, Predecessor[m]] ]
+
+(* Representation-independent defintion of Multiplication of natural numbers *)
+(*Add[n: NNPattern, Zero]:= n
+Add[n: NNPattern, m: NNPattern]:= Successor[ Add[n, Predecessor[m]] ]*)
+
+(* Realization of conceptual natural numbers through an abstract "NaturalNumbers" datatype *)
+If[$NaturalNumbersUseRealization, 
+	Zero:= NaturalNumber[0];
+	Successor[NaturalNumber[n_Integer]]:= NaturalNumber[n+1];
+	Predecessor[NaturalNumber[n_Integer]]:= NaturalNumber[n-1],
+	(*Else*)
+	Zero=.
+]
 
 End[]
 
